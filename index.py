6cd50569b26f99ded0014368bb97c5acf63b6db5
MontejoXga <mackan3-16@hotmail.com>
2023-03-04 07:47:56 -0500

    subida ultima

--- index.py
+++ index.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, request, jsonify
+from PIL import Image
+import numpy as np
+import cv2
+import re
+import base64
+import io 
+
 
 app=Flask(__name__)
 
@@ -6,8 +13,17 @@ app=Flask(__name__)
 
 ##Definimos ruta
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
+
+@app.route ('/detect', methods=["POST"])
+def disp_pic():
+
+    image_b64 = request.data
+    img_b64 = base64.b64encode(image_b64).decode('utf-8')
+    img_b64 = 'data:image/jpeg;base64, ' + img_b64
+    return jsonify(name='input.jpg', image=str(img_b64))
+    
 
 @app.route('/informacion')
 def informacion():
@@ -19,5 +35,5 @@ def consejos():
 
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5017)
+    app.run(debug=True)
 
--- static/ejecucion.js
+++ static/ejecucion.js
@@ -1,3 +1,6 @@
+
+var SERVER_URL = 'http://127.0.0.1:5000/detect';
+
 navigator.mediaDevices.getUserMedia(({audio:false, video: true}))
     .then((stream)=>{
         console.log(stream)
@@ -10,7 +13,6 @@ navigator.mediaDevices.getUserMedia(({audio:false, video: true}))
     })
 
 document.getElementById("takeSnapshot").addEventListener("click",()=>{
-    console.log("hola")
     document.querySelector(".foto").style.display = "grid";
     document.querySelector(".div_camera").style.display = "none";
     takePicture();
@@ -21,4 +23,32 @@ function takePicture() {
     const canvas = document.getElementById("canvasElement");
     let ctx = canvas.getContext('2d');
     ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+    var imgURL = canvas.toDataURL();
+    console.log(imgURL);
+    
+    $.ajax({
+      type: 'POST',
+      url: SERVER_URL,
+      data: imgURL,
+      timeout: 100 * 25,
+      contentType: false,
+      processData: false,
+      dataType: 'json',
+      success: function(data){
+        if(data.success){
+          alert('imagen enviada');
+        }else{
+          alert('hubo un error en el envio');
+        }
+      },
+      error: function(data){
+        alert('error');
+      }
+    }).done(function(){
+      
+      console.log("enviado");
+  
+    });
+
 }
+
